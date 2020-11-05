a80753f9c3687ecf476143d6bf567030308c3941
Yan Jiang <yanjiang@Yans-MacBook-Air.local>
2020-11-05 20:15:04 +0100

    Add test and travis-heroku deployment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test App component", () => {
+  test("renders without crashing", () => {
+    render(<App />);
+  });
+
+  test("renders link element 1", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Data Virtualization/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders link element 2", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Data Management/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders link element 3", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Export Data/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
